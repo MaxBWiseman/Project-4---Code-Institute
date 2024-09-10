d449e83f2586137ae1a1a59a53b656997961e152
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-10 19:03:35 +0000

    task: Add Post model to post_hub app models.py

--- post_hub/models.py
+++ post_hub/models.py
@@ -8,3 +8,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    blurb = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
