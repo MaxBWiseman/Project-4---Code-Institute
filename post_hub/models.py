3062b5ded7f84a6f067a95131302d4cd9eb83949
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-10 18:57:00 +0000

    task: Add Category model to post_hub app models.py

--- post_hub/models.py
+++ post_hub/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
