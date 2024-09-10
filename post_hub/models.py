31b14520397440d9f7777f8ca5fb6987fc634f46
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-10 19:23:33 +0000

    task: Add Comment model to post_hub app models.py and status field to Post model

--- post_hub/models.py
+++ post_hub/models.py
@@ -14,9 +14,23 @@ class Post(models.Model):
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     blurb = models.CharField(max_length=255)
     content = models.TextField()
+    status = models.BooleanField(default=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content
