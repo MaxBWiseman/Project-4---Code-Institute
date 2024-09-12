cfdd098862dcdc58e131e58738ad43eb204dbd60
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-12 13:51:42 +0000

    feat: Add total_upvotes and total_downvotes methods and slug decorator

--- post_hub/models.py
+++ post_hub/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
 from django.utils.text import slugify
 
 # Create your models here.
@@ -28,18 +30,32 @@ class Post(models.Model):
 # Each Post belongs to a single User and Category.
     
     def __str__(self):
-        return self.title
+        return f{self.title} by {self.author.username}
     
     def total_upvotes(self):
         return self.votes.filter(is_upvote=True).count()
 # I learned that count() is a method that returns the number of items in a queryset from
 # https://docs.djangoproject.com/en/5.1/ref/models/querysets/#count
+# "votes" is the related name of the ForeignKey in the Vote model.
     
     def total_downvotes(self):
         return self.votes.filter(is_upvote=False).count()
 # I dont include a is_downvote field in the Vote model as I can determine if a vote is a downvote
 # by checking if is_upvote is False.
-    
+
+@reciever(pre_save, sender=Post)
+def add_slug_to_post(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+# This automatically generates a slug for the post when it is created. Just before
+# the post is saved, the title of the post is slugified and saved as the slug.
+# "sender" means this should only be called for Post model instances.
+# args and kwargs are used to get any additional arguments that may be passed.
+# "instance" is the instance of the model that is being saved.
+# I learned that that signals can be used to perform actions when certain events occur,
+# for this case, I used the pre_save signal, this signal is sent just before the object is saved.
+# This decorator is used to connect the function to the signal.
+# https://stackoverflow.com/questions/8170704/execute-code-on-model-creation-in-django#:~:text=You%20can%20use%20django%20signals%20%27%20post_save%3A%20%23,MyModel%28models.Model%29%3A%20pass%20def%20my_model_post_save%28sender%2C%20instance%2C%20created%2C%20%2Aargs%2C%20%2A%2Akwargs%29%3A
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
