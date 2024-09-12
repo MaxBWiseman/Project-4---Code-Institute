9b0311434e794f5fe6398f3e0ef6d546544093b8
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-12 14:50:54 +0000

    refactor: Add SummernoteModelAdmin to PostAdmin in admin.py

--- post_hub/admin.py
+++ post_hub/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Admin class for the Post model.
+    This class is used to configure the Post model in the Django admin interface.
+    
+    **Context**
+    
+    ``PostAdmin``
+        An instance of :model:`blog.Post` that represents the Post model in the Django admin interface.
+   
+      
+    """
+    list_display = ('title', 'slug', 'status', 'date_posted')
+    search_fields = ['title', 'content']
+    list_filter = ('status','date_posted')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
+admin.site.register(Comment)
