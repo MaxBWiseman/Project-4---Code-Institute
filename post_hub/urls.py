5d8f68b8f0c686fd5fa95a1eee6812d3d8f8a23c
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-12 15:37:33 +0000

    task:: make URLs in post_hub/urls.py and made migrations

--- post_hub/urls.py
+++ post_hub/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('categories/', views.category_list, name='category_list'),
+    path('category/<str:name>/', views.category_detail, name='category_detail'),
+]
