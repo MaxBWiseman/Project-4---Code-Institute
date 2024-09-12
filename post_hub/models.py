f6ed39530a6eb9821a641055712fcd2ae7818148
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-12 13:10:21 +0000

    feat: Added Vote, Comment, Post, Category, and Profile models with comments to post_hub app models.py

--- post_hub/models.py
+++ post_hub/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+# Category model is used to store the categories of the posts.
+# Each Category has a unique name.
+# This has a many to one relationship with the Post model.
 
     def __str__(self):
         return self.name
@@ -19,6 +22,9 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+# Post model has a many to one relationship with the User and Category models,
+# this is to store the posts of the users in the categories.
+# Each Post belongs to a single User and Category.
     
     def __str__(self):
         return self.title
@@ -31,6 +37,8 @@ class Comment(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+# Comment model has a many to one relationship with the Post and User models,
+# this is to store the comments of the users on the posts.
     
     def __str__(self):
         return self.content
@@ -39,6 +47,20 @@ class Vote(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='votes')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_upvote = models.BooleanField()
+# Vote model has a many to one relationship with the Post and User models,
+# this is to store the votes of the users on the posts.
     
     def __str__(self):
         return self.post.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+# Profile model has a one to one relationship with the User model,
+# this is to store additional information about the user.
+    
+    def __str__(self):
+        return self.user.username
+# Profile model added as I included it in my database diagram, this is a suggestion.
