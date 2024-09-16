02f665b4b3130d5b86a86de71905b19a2451a302
Max Wiseman <maxwise70@hotmail.co.uk>
2024-09-16 10:47:43 +0000

    chore: created a test for the comment form

--- post_hub/tests.py
+++ post_hub/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from .forms import PostForm
+from django.contrib.auth.models import User
+from .forms import PostForm, CommentForm
+from .models import Comment, Post, Category
 
-class PostFormTest(TestCase):
+class PostFormTest4SpellChecker(TestCase):
     def test_spell_checker(self):
         # Test with a misspelled category name "technology"
         form_data = {
@@ -31,3 +33,50 @@ class PostFormTest(TestCase):
         form_data['new_category'] = 'development'
         form = PostForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+class CommentFormTest(TestCase):
+    def setUp(self):
+        # Create a user for the author
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        
+        # Create a Category for the post
+        self.category = Category.objects.create(category_name='Test Category')
+
+         # Create a post for the comment
+        self.post = Post.objects.create(title='Test Post', blurb='Test Blurb', content='Test Content', category=self.category , author=self.user)
+        
+         # Create a parent comment for testing
+        self.parent_comment = Comment.objects.create(content="Parent comment", author=self.user, post=self.post)
+        
+    def test_comment_form_valid(self):
+        # Test with valid data
+        form_data = {
+            'content': 'This is a test comment.',
+            'parent': self.parent_comment.id,
+            'author': self.user.id,
+            'post': self.post.id
+        }
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_no_parent(self):
+        # Test with valid data but no parent
+        form_data = {
+            'content': 'This is a test comment without parent.',
+            'author': self.user.id,
+            'post': self.post.id
+        }
+        form = CommentForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_empty_content(self):
+        # Test with empty content
+        form_data = {
+            'content': '',
+            'parent': self.parent_comment.id,
+            'author': self.user.id,
+            'post': self.post.id
+        }
+        form = CommentForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors)
